5fab46807933000d7d8ffc907d0a5c46ca9d8cc9
Nikola Gajski <nikola.gajski@okultis.com>
2026-03-05 20:52:50 +0100

    improve logos

--- src/data/clients.ts
+++ src/data/clients.ts
@@ -1,10 +1,10 @@
-import kinana from '@/assets/images/clients/kinana.webp'
-import blooming from '@/assets/images/clients/blooming.webp'
-import posluzIme from '@/assets/images/clients/posluz_ime.webp'
-import bjuti from '@/assets/images/clients/bjuti.webp'
-import greenSolutions from '@/assets/images/clients/GreenSolutions.webp'
-import auroraMedia from '@/assets/images/clients/Aurora-Media-logo.webp'
+import kinana from '@/assets/images/clients/kinana.png'
+import blooming from '@/assets/images/clients/blooming.svg'
+import greenSolutions from '@/assets/images/clients/green_solutions.png'
+import auroraMedia from '@/assets/images/clients/Aurora Media logo-06.png'
 import mbAutoskola from '@/assets/images/clients/mb_autoskola.png'
+import zmda from '@/assets/images/clients/zmda.png'
+import posluzIme from '@/assets/images/clients/posluzi.me.png'
 
 export interface Client {
   src: string
@@ -14,9 +14,9 @@ export interface Client {
 export const clients: Client[] = [
   { src: kinana, alt: 'Kinana' },
   { src: blooming, alt: 'Blooming' },
-  { src: posluzIme, alt: 'posluzi.me' },
-  { src: bjuti, alt: 'Bjuti' },
   { src: greenSolutions, alt: 'GreenSolutions' },
   { src: auroraMedia, alt: 'Aurora Media' },
   { src: mbAutoskola, alt: 'MB Autoskola' },
+  { src: zmda, alt: 'ZMDA' },
+  { src: posluzIme, alt: 'posluzi.me' },
 ]
